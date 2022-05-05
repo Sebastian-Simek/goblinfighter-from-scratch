bc3bbf404448fa6e2130f25bcce166837edeaaba
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-04 21:04:14 -0700

    added initial objects to be displayed

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const goblinList = document.getElementById('goblin-display');
 const playerHPDisplay = document.getElementById('player HP');
 const defeatedGoblins = document.getElementById('defeated-goblins');
 
-let goblinArray = [];
+let goblinArray = [{ name: 'Professor Oak', HP: 7 }, { name: 'Eddie', HP: 4 }];
 let playerHP = 10;
 let goblinsSlain = 0;
 // set event listeners 
+displayGoblin();
+
 addGoblin.addEventListener('click', () => {
     let goblinObject = {
         name: newGoblin.value,
@@ -32,18 +34,17 @@ function displayGoblin() {
 
 function goblinClickHandler(booger) {
     const randomNum = Math.random();
-    // if (booger.HP <= 0) return;
+    if (booger.HP <= 0) return;
     if (playerHP === 0) return alert('you lose');
-    if (randomNum < .5) {
+    if (randomNum < 1) {
         booger.HP--;
         alert('you have hit the goblin');
-        console.log(goblinsSlain);
     } 
     if (booger.HP === 0) {
         goblinsSlain++;
         defeatedGoblins.textContent = `You have slain ${goblinsSlain} goblin(s)`;
     }
-    if (randomNum > .5) {
+    if (randomNum > 5) {
         playerHP--;
         alert('you have missed');
         playerHPDisplay.textContent = `you have ${playerHP} HP`;
